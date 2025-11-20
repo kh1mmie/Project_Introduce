823890c45fe46665fd328835c3dd2a1ed79b085b
unknown <khimmie40@gmail.com>
2025-11-20 13:35:45 +0700

    newDesign

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,4 @@
-import { useState, useRef, useEffect, use } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-// library.add(faAngleDown);
+import { useState, useRef, useEffect } from "react";
 
 export default function Navbar() {
   const aboutRef = useRef(null);
@@ -53,9 +48,9 @@ export default function Navbar() {
             <a
               href="#"
               onClick={() => handleMenuClick("home")}
-              className={`${
+              className={`nav-link ${
                 selectedMenu === "home"
-                  ? "text-Green4 border-b-2 border-Green4 transition-discrete duration-200 scale-110 inline-block "
+                  ? "nav-active"
                   : "hover:text-white hover:scale-110 inline-block transition-transform duration-200"
               }`}
             >
@@ -68,65 +63,24 @@ export default function Navbar() {
             <a
               href="#"
               onClick={() => handleMenuClick("What I do")}
-              className={`${
+              className={`nav-link ${
                 selectedMenu === "What I do"
-                  ? "text-Green4 border-b-2 border-Green4 scale-110 inline-block "
+                  ? "nav-active "
                   : "hover:text-white hover:scale-110 inline-block transition-transform duration-200"
               }`}
             >
               What I do
             </a>
           </li>
-          {/* <li ref={aboutRef} className="relative justify-items-end">
-            <a href="#"
-              type="button"
-              id="about-button"
-              onClick={() => handleMenuClick("about")}
-              aria-haspopup="true"
-              aria-expanded={selectedMenu === "about"}
-              aria-controls="about-menu"
-              className={`flex items-center ${
-                selectedMenu === "about"
-                  ? "text-[#A3FE01] border-b-2 border-[#A3FE01]"
-                  : "hover:text-white"
-              }`}
-            >
-              <span className="mr-1.5"> About me </span>
-
-              {/* DropDown about */}
-          {/* <FontAwesomeIcon
-                icon={faAngleDown}
-                className={`${
-                  selectedMenu === "about" ? "rotate-180" : "rotate-0"
-                } transition-transform duration-200`}
-              />
-            </a>
-
-            {selectedMenu === "about" && (
-              <div
-                id="about-menu"
-                role="menu"
-                className="absolute mt-2 w-35 
-              bg-white/7 text-white/40 rounded-md shadow-lg z-10 border-1 border-white/20 "
-              >
-                <a href="#" className="block px-4 py-2 hover:text-Green4 hover:bg-white/5">
-                  Profile me
-                </a>
-                <a href="#" className="block px-4 py-2 hover:text-Green4 hover:bg-white/5">
-                  What I do
-                </a>
-              </div>
-            )}
-          </li> */}
 
           {/* 3-Work */}
           <li>
             <a
               href="#"
               onClick={() => handleMenuClick("work")}
-              className={`${
+              className={`nav-link ${
                 selectedMenu === "work"
-                  ? "text-[#A3FE01] border-b-2 border-[#A3FE01] scale-110 inline-block "
+                  ? "nav-active "
                   : "hover:text-white hover:scale-110 inline-block transition-transform duration-200"
               }`}
             >
